c25e17f6fd198fab4eda9a05dc308482d0bf690c
johan <johankuijt@gmail.com>
2014-03-26 17:37:55 +0100

    Multiple db support

--- config/module.config.php
+++ config/module.config.php
@@ -1,11 +1,12 @@
 <?php
 /**
- * This file is part of the MQRedisSessionStorage Module (https://github.com/milqmedia/MQRedisSessionStorage.git)
- *
- * Copyright (c) 2013 Milq Media (https://github.com/milqmedia/MQRedisSessionStorage.git)
+ * Copyright (c) 2013 Milq Media (https://github.com/milqmedia)
  *
  * For the full copyright and license information, please view
  * the file LICENSE.txt that was distributed with this source code.
+ * 
+ * @author Milq Media <johan@milq.nl>
+ * 
  */
 return array(
     'service_manager' => array(
